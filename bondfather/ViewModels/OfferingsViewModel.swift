4e18b608af898a35edd4940f88f7d7d55a3494cc
Ayush Sachdeva <ayush.sachdeva@pipedrive.com>
2026-05-07 22:07:29 +0300

    feat: add OfferingsViewModel

---
+++ bondfather/ViewModels/OfferingsViewModel.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+enum LoadState {
+    case loading
+    case loaded([Offering])
+    case error
+}
+
+@Observable
+final class OfferingsViewModel {
+    var loadState: LoadState = .loading
+    private let service = OfferingService()
+
+    func load() async {
+        loadState = .loading
+        do {
+            let offerings = try await service.fetchOfferings()
+            loadState = .loaded(offerings.sorted {
+                switch ($0.subscriptionStart, $1.subscriptionStart) {
+                case let (a?, b?): return a < b
+                case (nil, _): return false
+                case (_, nil): return true
+                }
+            })
+        } catch {
+            loadState = .error
+        }
+    }
+}
