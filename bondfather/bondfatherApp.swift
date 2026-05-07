7f15c777650132696978f9988d7ff82d535b04f9
Ayush Sachdeva <ayush.sachdeva@pipedrive.com>
2026-05-07 21:30:33 +0300

    Initial Commit

---
+++ bondfather/bondfatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  bondfatherApp.swift
+//  bondfather
+//
+//  Created by Ayush Sachdeva on 07.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct bondfatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
