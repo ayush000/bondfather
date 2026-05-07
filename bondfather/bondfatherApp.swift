9735d211f369b1e1e6abf044c6585ade9ccbe339
Ayush Sachdeva <ayush.sachdeva@pipedrive.com>
2026-05-07 22:09:28 +0300

    feat: wire up app entry point, background task, and Info.plist

--- bondfather/bondfatherApp.swift
+++ bondfather/bondfatherApp.swift
@@ -1,17 +1,21 @@
-//
-//  bondfatherApp.swift
-//  bondfather
-//
-//  Created by Ayush Sachdeva on 07.05.2026.
-//
-
 import SwiftUI
 
 @main
 struct bondfatherApp: App {
+    private let backgroundTaskManager = BackgroundTaskManager()
+    private let notificationManager = NotificationManager()
+
+    init() {
+        backgroundTaskManager.registerBackgroundTask()
+    }
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            OfferingsListView()
+                .task {
+                    await notificationManager.requestPermission()
+                    backgroundTaskManager.scheduleNextRefresh()
+                }
         }
     }
 }
